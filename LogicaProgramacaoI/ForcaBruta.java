81fdf812817ca2d55225fb528b3d8af05e565e6c
edumagueta <edumagueta@gmail.com>
2024-12-30 20:19:09 -0300

    Commit dos exercicios do repo clonado

---
+++ LogicaProgramacaoI/ForcaBruta.java
@@ -0,0 +1,37 @@
+package LogicaProgramacaoI;
+
+import java.util.Scanner;
+
+public class ForcaBruta {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma senha de 4 dígitos númericos: ");
+        int senha = scanner.nextInt();
+
+        if (senha < 0 || senha > 9999) {
+            System.out.println("Senha inválida. Digite novamente 4 dígitos numéricos.");
+            return;
+        }
+
+        long inicio = System.currentTimeMillis();
+        int tentativas = 0;
+        int senhaQuebrada = -1;
+
+        for (int i = 0; i <= 9999; i++){
+            tentativas++;
+            if (i == senha){
+                senhaQuebrada = i;
+                break;
+            }
+        }
+
+        long fim = System.currentTimeMillis();
+
+        System.out.println("Senha encontrada: " + senhaQuebrada);
+        System.out.println("Número de tentativas: " + tentativas);
+        System.out.println("Tempo total gasto: " + (fim - inicio) + " ms");
+
+        scanner.close();
+    }
+}
